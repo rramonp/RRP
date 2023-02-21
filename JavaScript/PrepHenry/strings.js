bca06470e41cd5f675b2d4e9c0d476f9457432fe
rrp <bakalap2@gmail.com>
2023-02-21 01:41:06 +0100

    testing

--- JavaScript/PrepHenry/strings.js
+++ JavaScript/PrepHenry/strings.js
@@ -27,3 +27,32 @@ console.log('Using search() for first ocurrence of \'because\' ', sentence.searc
 
 let challenge_spaced = '        30 Days of Javascript  '
 console.log(`[${challenge_spaced}] now trimmed with trim()--> [${challenge_spaced.trim()}]`)
+console.log(challenge.startsWith(3))
+console.log(challenge.startsWith('30'))
+console.log(challenge.endsWith('t'))
+console.log(challenge.endsWith('Javascript'))
+console.log(challenge.match('a'))
+let word = 'Javascript'
+console.log('Word:', word, 'Concatenated with phrase', challenge.concat(word))
+console.log('Using repeat()', challenge.repeat(2))
+
+console.log('The quote: \'There is no exercise better for the heart than reaching down and lifting people up\' John Holmes teaches us to help one another')
+let numberstr = '10'
+let strtonumber = parseInt(numberstr)
+console.log(typeof(strtonumber))
+let list2 = 'Python, Argon'
+console.log(list2.search('on'))
+
+let randomnumber = Math.random(50,100)
+let random2= randomnumber * 100
+console.log(random2)
+console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27')
+let new_phrase = 'You cannot end a sentence with because because because is a conjunction'
+console.log(new_phrase.indexOf('because'))
+console.log(new_phrase.substr(31, 7))
+let phrasetocount = 'love is the best thing in this world. Some found their love and some are still looking for their love'
+let matches = phrasetocount.match(pattern)
+console.log(matches)
+
+
+
